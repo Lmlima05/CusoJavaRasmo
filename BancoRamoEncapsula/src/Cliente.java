81967e860b3ef48764bd0d8a7479bce46de47250
Luan <luanmelolima@gmail.com>
2025-03-12 15:12:34 -0300

    Finalizando a parte de get e set

--- BancoRamoEncapsula/src/Cliente.java
+++ BancoRamoEncapsula/src/Cliente.java
@@ -1,9 +1,39 @@
 public class Cliente {
 
-  String nome;
-  String cpf;
-  String profissao;
-  double salario;
-  double saldo;
-  
+  private String nome;
+  private String cpf;
+  private String profissao;
+  private double salario;
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getProfissao() {
+    return profissao;
+  }
+
+  public void setProfissao(String profissao) {
+    this.profissao = profissao;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
 }
--- BancoRamoEncapsula/src/TesteEncapsulamento.java
+++ BancoRamoEncapsula/src/TesteEncapsulamento.java
@@ -1,9 +1,27 @@
 public class TesteEncapsulamento {
     public static void main(String[] args) {
+ //      Cliente cliente = new Cliente();
+//        cliente.setNome("Luan");
+//        cliente.setCpf("123456789");
+//        cliente.setProfissao("Desenvolvedor");
+//        cliente.setSalario(10000);
+
         Conta conta = new Conta();
         conta.depositar(1000);
-        conta.sacar(1000);
+        conta.sacar(1500);
+        conta.setAgencia(10);
 
+        conta.setTitular(new Cliente());
+        conta.getTitular().setNome("Gustavo");
+        conta.getTitular().setCpf("123456789");
+        conta.getTitular().setProfissao("Desenvolvedor");
+        conta.getTitular().setSalario(10000);
+
+        System.out.println(conta.getTitular());
+        System.out.println(conta.getTitular().getNome());
+        System.out.println(conta.getTitular().getProfissao());
+        System.out.println(conta.getTitular().getSalario());
         System.out.println(conta.getSaldo());
+
     }
 }
